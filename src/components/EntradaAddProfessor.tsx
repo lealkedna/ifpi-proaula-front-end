33bf7816edf05cf7b490d981072deaf7f3144052
Kedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2022-11-11 13:30:06 -0300

    entrada profesor

---
+++ src/components/EntradaAddProfessor.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+interface EntradaProfessorProps {
+  tipo?: 'text' | 'number'
+  texto: string
+  valor: string
+  somenteLeitura?: boolean
+  className?: string
+
+  valorMudou?: (valor: string) => void
+}
+
+export default function EntradaProfessor(props: EntradaProfessorProps) {
+  return (
+    <div
+      className={`
+        flex flex-col ${props.className}
+        `}
+    >
+      <label className="mb-2">{props.texto}</label>
+      <input
+        type={props.tipo ?? 'text'}
+        value={props.valor}
+        readOnly={props.somenteLeitura}
+        onChange={(e) => props.valorMudou?.(e.target.value)}
+        className={`
+             border border-purple-500 rounded-1g
+             bg-white focus:outline-none px-2 py-2
+            `}
+      />
+    </div>
+  )
+}
