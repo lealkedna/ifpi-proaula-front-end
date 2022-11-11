e545bb9a1cd23b442c73067635b72b0b3bc273ba
Lukas00-gif <lucassky08@gmail.com>
2022-11-11 13:06:22 -0300

    tabela Professores add

--- src/pages/professores.tsx
+++ src/pages/professores.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
 
 import Layout from '../components/Layout'
+import TabelaProfessores from '../components/TabelaProfessores'
+import TodosProfessores from '../core/TodosProfessores'
 
 export default function Professores() {
   // esse e a tela dos professoes lembrando que e so colocar o /professores na url
+
+  const professores = [
+    new TodosProfessores(
+      '1',
+      'Jesiel',
+      'Jesiel@ifpi.com.br',
+      '12345ER',
+      'Professor',
+      'ADS'
+    ),
+    new TodosProfessores(
+      '2',
+      'Aislan',
+      'Aislan@ifpi.com.br',
+      '12345UY',
+      'Professor',
+      'ADS'
+    ),
+    new TodosProfessores(
+      '3',
+      'Jader',
+      'Jader@ifpi.com.br',
+      '12345OP',
+      'Professor',
+      'ADS'
+    ),
+  ]
+
+  function professorSelecionado(professor: TodosProfessores) {
+    console.log(professor.nome)
+  }
+
+  function professorExcluido(professor: TodosProfessores) {
+    console.log(`o professor ${professor.nome} foi excluido`)
+  }
+
   return (
     <div
       className={`
@@ -12,7 +50,11 @@ export default function Professores() {
           `}
     >
       <Layout titulo="HU ?? - Professores - ADM">
-        <span>conteudo</span>
+        <TabelaProfessores
+          professores={professores}
+          professorSelecionado={professorSelecionado}
+          professorExcluido={professorExcluido}
+        />
       </Layout>
     </div>
   )
